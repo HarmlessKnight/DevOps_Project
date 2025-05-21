bd7e9104c6401e14d0b43b9fd353ff767bfdbca8
HarmlessKnight <HarmlessKnight@users.noreply.github.com>
2025-05-21 20:26:34 +0200

    removed unlogged

--- Personal_finance_tracker/src/main/java/com/example/personal_finance_tracker/PersonalFinanceTrackerApplication.java
+++ Personal_finance_tracker/src/main/java/com/example/personal_finance_tracker/PersonalFinanceTrackerApplication.java
@@ -1,13 +1,12 @@
 package com.example.personal_finance_tracker;
 
-import io.unlogged.Unlogged;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonalFinanceTrackerApplication {
-
-    @Unlogged
+    
     public static void main(String[] args) {
         SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
     }
